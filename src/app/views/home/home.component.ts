f895e6791a8e8f0961f9ea8817b27f071bfa4d5e
KieranMueller <kieran98mueller@gmail.com>
2025-03-11 14:29:02 -0500

    progress

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CarouselComponent } from '../../components/carousel/carousel.component'
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [CarouselComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+    ngOnInit(): void {
+
+    }
+
+    socialRoute(location: 'facebook' | 'instagram') {
+        switch (location) {
+            case 'facebook': {
+                window.open('https://www.facebook.com/fleifly/', '_blank')
+                break
+            }
+            case 'instagram': {
+                window.open('https://www.instagram.com/fleiglesflyers/?hl=en', '_blank')
+                break
+            }
+        }
+    }
 }
