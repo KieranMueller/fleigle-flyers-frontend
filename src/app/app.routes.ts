c1ce4cfca470e2c93bdbf63f2f2b3c2a54750ff1
KieranMueller <kieran98mueller@gmail.com>
2025-03-11 13:25:55 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from './views/home/home.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+]
