e89a55e013763a4f5cb4116d3f640d35e72e8c7e
KieranMueller <kieran98mueller@gmail.com>
2025-03-11 20:27:30 -0500

    stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './views/home/home.component'
+import { BiosComponent } from './views/bios/bios.component'
+import { RegistrationComponent } from './views/registration/registration.component'
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    { path: 'bios', component: BiosComponent, pathMatch: 'full' },
+    { path: 'register', component: RegistrationComponent, pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ]
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
-  selector: 'app-nav',
-  standalone: true,
-  imports: [],
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+    selector: 'app-nav',
+    standalone: true,
+    imports: [],
+    templateUrl: './nav.component.html',
+    styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+    constructor(private router: Router) { }
 
+    navigate(destination: 'home' | 'bio' | 'registration') {
+        console.log(destination)
+        switch (destination) {
+            case 'home': {
+                this.router.navigateByUrl('/home')
+                break
+            }
+            case 'bio': {
+                this.router.navigateByUrl('/bios')
+                break
+            }
+            case 'registration': {
+                this.router.navigateByUrl('/register')
+                break
+            }
+        }
+    }
 }
