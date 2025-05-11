7ded02c8c4434da6b13dcc5899b7e2bfb8b8f968
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2025-05-10 22:24:05 -0500

    added google calendar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { BiosComponent } from './views/bios/bios.component'
 import { RegistrationComponent } from './views/registration/registration.component'
 import { CoachesComponent } from './views/coaches/coaches.component'
 import { AboutComponent } from './views/about/about.component'
+import { EventsComponent } from './views/events/events.component'
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'register', component: RegistrationComponent, pathMatch: 'full' },
     { path: 'coaches', component: CoachesComponent, pathMatch: 'full' },
     { path: 'about', component: AboutComponent, pathMatch: 'full' },
+    { path: 'events', component: EventsComponent, pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ]
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,7 +1,8 @@
 import { Component, HostListener, OnInit } from '@angular/core'
 import { GlobalStateService } from '../../shared/global-state.service'
 import { NavigateService } from '../../shared/navigate.service'
-import { RouterModule } from '@angular/router'
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router'
+import { filter } from 'rxjs'
 
 @Component({
     selector: 'app-nav',
@@ -12,10 +13,12 @@ import { RouterModule } from '@angular/router'
 })
 export class NavComponent implements OnInit {
     dropdownOpen = false
+    home = ['Home', 'home']
     links: any = []
     _links = new Map<string, string>()
+    onHomePage = false
 
-    constructor(public globalState: GlobalStateService, public navService: NavigateService) { }
+    constructor(public globalState: GlobalStateService, public navService: NavigateService, private route: Router) { }
 
     ngOnInit() {
         this.globalState.setMobile(window.innerWidth)
@@ -28,7 +31,12 @@ export class NavComponent implements OnInit {
         this._links.set('Resources', 'resources')
         this._links.set('Register', 'register')
         this.links = Array.from(this._links.entries())
-        console.log(this.links)
+        this.route.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event: any) => {
+                if (event.urlAfterRedirects === '/home') this.onHomePage = true
+                else this.onHomePage = false
+            });
     }
 
     @HostListener('click', ['$event'])
